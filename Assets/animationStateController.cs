8529a8c123865824719d124620f3d7496f023c9a
nico-mania <55563178+nico-mania@users.noreply.github.com>
2022-01-31 16:50:36 +0100

    21-newanimations:
    added strafe animation
    fixed sneak bug

--- Assets/animationStateController.cs
+++ Assets/animationStateController.cs
@@ -10,6 +10,8 @@ public class animationStateController : MonoBehaviour
     private int isRunningBackHash;
     private int isSneakingHash;
     private int isJumpingHash;
+    private int isStrafeRHash;
+    private int isStrafeLHash;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,8 @@ public class animationStateController : MonoBehaviour
         isRunningBackHash = Animator.StringToHash("isRunningBack");
         isSneakingHash = Animator.StringToHash("isSneaking");
         isJumpingHash = Animator.StringToHash("isJumping");
+        isStrafeLHash = Animator.StringToHash("isStrafeLeft");
+        isStrafeRHash = Animator.StringToHash("isStrafeRight");
     }
 
     // Update is called once per frame
@@ -25,10 +29,14 @@ public class animationStateController : MonoBehaviour
     {
         bool isRunning = animator.GetBool("isRunning");
         bool isRunningBack = animator.GetBool("isRunningBack");
+        bool isStrafeL = animator.GetBool("isStrafeLeft");
+        bool isStrafeR = animator.GetBool("isStrafeRight");
         bool forwardPressed = Input.GetKey("w");
         bool backwardPressed = Input.GetKey("s");
         bool sneakPressed = Input.GetKey("left shift");
         bool jumpPressed = Input.GetKeyDown("space");
+        bool strafeLPressed = Input.GetKey("a");
+        bool strafeRPressed = Input.GetKey("d");
         
         //running forward
         if (!isRunning && forwardPressed)
@@ -38,6 +46,23 @@ public class animationStateController : MonoBehaviour
         if (isRunning && !forwardPressed)
         {
             animator.SetBool(isRunningHash, false);
+        }
+        //strafe left
+        if (!isStrafeL && strafeLPressed)
+        {
+            animator.SetBool(isStrafeLHash, true);
+        }
+        if (isStrafeL && !strafeLPressed)
+        {
+            animator.SetBool(isStrafeLHash, false);
+        }//strafe right
+        if (!isStrafeR && strafeRPressed)
+        {
+            animator.SetBool(isStrafeRHash, true);
+        }
+        if (isStrafeR && !strafeRPressed)
+        {
+            animator.SetBool(isStrafeRHash, false);
         }
         //running backwards
         if (!isRunningBack && backwardPressed)
@@ -53,7 +78,7 @@ public class animationStateController : MonoBehaviour
         {
             animator.SetBool(isSneakingHash, true);
         }
-        if (forwardPressed && !sneakPressed)
+        if ((forwardPressed && !sneakPressed) ||(!forwardPressed && sneakPressed))
         {
             animator.SetBool(isSneakingHash, false);
         }
