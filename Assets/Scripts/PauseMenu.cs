92f9f7266515273e245e9f3a4eb3424e6e1a15d9
nico-mania <55563178+nico-mania@users.noreply.github.com>
2022-02-03 16:27:39 +0100

    23-ui:
    added new font
    added quality and resolution dropdown

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -31,7 +31,7 @@ public class PauseMenu : MonoBehaviour
         int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+            string option = resolutions[i].width + " x " + resolutions[i].height + " " + resolutions[i].refreshRate + "hz";
             options.Add(option);
 
             if (resolutions[i].width == Screen.currentResolution.width &&
